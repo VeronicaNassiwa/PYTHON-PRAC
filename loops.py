63dab85b1ab9c2142523240e795e2792ac87679a
VeronicaNassiwa <veronicanassiwa@gmail.com>
2022-02-22 17:16:53 +0400

    CH[Fixers #181319442]:Boiler plate
    In this commit there is ways of usinf loops
    in python. Just for study purposes

--- loops.py
+++ loops.py
@@ -1,3 +1,33 @@
+from unicodedata import name
+
+
 mynumbers=[4,5,6,78,9,0,4,]
-for number in mynumbers:
-    print(number)
+#for number in mynumbers:
+    #print(number)
+#user_wants_acar=True
+#if user_wants_acar==True:
+    #print("Lambogin")
+#user_wants_acar==False
+
+#user_wants_anotherchoice=input("You want another car? y/n")
+
+#if user_wants_anotherchoice  == 'y':
+    #print("{toyota,jeep,corona}")
+#else:
+    #print("welaba otulabyeko nyo")
+
+#my_freinds={"Nyla","Betty","Prossy","Winnie","Habib","Famba"}
+
+#name= input("Enter your freinds ")
+#if name  in my_freinds:
+   # print("{} is Vero's freind".format(name))
+#else:
+    #print("{} is not Vero'sfreind".format(name))   
+        
+
+def mypeople():
+     mypeoplee=input("enter the people you know")
+     my_list0f_people=mypeoplee.split(",") 
+     return my_list0f_people
+mypeople()
+      
