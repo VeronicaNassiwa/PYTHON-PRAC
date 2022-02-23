2e3ccbc891aa7427781812154e45074750004e48
VeronicaNassiwa <veronicanassiwa@gmail.com>
2022-02-23 15:39:48 +0400

    CH[Fixers #181319442]:Adding loops to dictionaies
    Here i have just added an if statement on my
    stusent dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -1,8 +1,20 @@
+from turtle import clear
+from unicodedata import name
+
+
 def students():
     student_1= {'name': 'Veronica',
     'age':'50',
     'religion':'Islam',
     'coursunits':['basicmath','computer ethics','database programming','softwareprogramming']}
     #return student_1['coursunits']
-    print(student_1['coursunits'])
-students()
+    #print(student_1['coursunits'])
+    student_name=input("Enter the student name: ")
+    if  student_name==student_1['name']:
+        print("{} is enlolled".format(student_1['name']) )
+    else:
+        print("{} is not studing from here!".format(student_name))    
+
+students() 
+
+
